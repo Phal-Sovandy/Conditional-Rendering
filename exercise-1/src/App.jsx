44967e2d9c7a078a1236372b356b48eb0219ead6
Phal Sovandy <sovandy.phal@student.cadt.edu.kh>
2025-03-11 17:39:17 +0700

    Finish By Adding Header Component

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -1,14 +1,11 @@
-import pnLogo from "./assets/pn-logo.png";
 import Scores from "./components/Scores.jsx";
 import {HTML_RESULTS} from './data.js'
+import Header from "./components/Header.jsx"
 
 function App() {
   return (
     <>
-      <header id="header">
-        <img src={pnLogo} alt="PN Logo" />
-        <h1>Students results for (fake batch name)</h1>
-      </header>
+      <Header name="Ah Poy"/>
 
       <main className="scores-container">
         <Scores courseName="HTML" courseResults={HTML_RESULTS}/>
